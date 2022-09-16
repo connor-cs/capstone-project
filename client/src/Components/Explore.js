d89fb5f1bac0f2bbccf9a932b88120fc186008f3
Connor <connor.cs@protonmail.com>
2022-09-16 11:17:48 -0400

    added more explore form stuff

--- client/src/Components/Explore.js
+++ client/src/Components/Explore.js
@@ -1,19 +1,39 @@
 import React from 'react'
+import { useState } from 'react'
 import SelectUSState from 'react-select-us-states'
 
 export default function Explore() {
 
+  const [userSearch, setUserSearch] = useState(
+    {
+      city: '',
+      state: '',
+      distance: ''
+    }
+  )
+ 
 
+  function handleSubmit(e) {
+    // e.preventDefault()
+    console.log('sumbitted')
+    
+    fetch('/')
+  }
 
 
   return (
     <main className='explore-page'>
       <div>
-        <form>
-          <input type="text" placeholder="City"></input>
+        <form onSubmit={handleSubmit()}>
+          
+          <input type="text" value = "city" placeholder="City"></input>
+          
           <SelectUSState placeholder="state" />
+          
           <label>Max distance:</label>
           <input type="number"></input>
+          
+          <button>submit</button>
         </form>
       </div>
     </main>
